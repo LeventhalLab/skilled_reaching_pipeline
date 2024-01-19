fd56a02c96c8da66e251fa844c74b2f02cfc3c41
Dan Leventhal <dleventh@med.umich.edu>
2024-01-18 19:34:44 -0500

    Update skilled_reaching_workflow.py

--- skilled_reaching_workflow.py
+++ skilled_reaching_workflow.py
@@ -528,7 +528,7 @@ if __name__ == '__main__':
         # for DLC_key in DLC_folder_keys:
         #     train_autoencoder.train_autoencoder(anipose_config, DLC_folder_names[DLC_key])
         # ftr = [folder for folder in folders_to_reconstruct if ((folder['ratID'] == 'R0486') and (folder['date'] == datetime(2023, 9, 8)))]
-        ftr = [folder for folder in folders_to_reconstruct if not folder['ratID'] in ['R0452', 'R0453', 'R0469', 'R0470', 'R0471']]
+        ftr = [folder for folder in folders_to_reconstruct if not folder['ratID'] in ['R0452', 'R0453', 'R0468', 'R0469', 'R0470', 'R0471', 'R0472']]
         # ftr = folders_to_reconstruct
         reconstruct_3d.reconstruct_folders_anipose(ftr, parent_directories[expt], expt, rat_df, anipose_config, filtered=False)
 
